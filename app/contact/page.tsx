05c2de77bf527b64388ac44373040e4a7b6ab08d
printcess <pngerdchai@gmail.com>
2025-12-07 12:45:57 +0700

    contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,8 +5,7 @@ export default function Contact() {
   return (
     <main className="min-h-screen bg-slate-800 text-white pt-24 p-8">
       <Navbar />
-      <h1 className="text-4xl font-bold mb-4">Contact</h1>
-      <p>Contact list goes here...</p>
+      
       <Footer />
     </main>
   );
